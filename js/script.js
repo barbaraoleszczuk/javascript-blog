be43bcd3a37ea071bf179d79f2c12f110a622d23
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-02-03 18:20:36 +0100

    Add active articles

--- js/script.js
+++ js/script.js
@@ -3,42 +3,50 @@
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+
+  console.log(event);
+  console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
-
+  
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+   /* [IN PROGRESS] add class 'active' to the clicked link */
+
   clickedElement.classList.add('active');
+
   console.log('clickedElement:', clickedElement);
 
-  /* [DONE] remove class 'active' from all articlES */
-  const activeArticles = document.querySelectorAll('.post .active');
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts .active');
+
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  
-  
   /* get 'href' attribute from the clicked link */
+
+  const articleAttribute = clickedElement.getAttribute('href');
   
- const articleAttribiute = clickedElement.getAttribute('href');
- console.log("Herf:" ,articleAttribiute);
+  console.log(articleAttribute);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle= document.querySlelctor(articleAttribiute)
-  console.log('Html article selector: ', targetArticle);
+
+  const targetArticle = document.querySelector(articleAttribute);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+
   targetArticle.classList.add('active');
+
 }
-
+  
 const links = document.querySelectorAll('.titles a');
-
+  
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
